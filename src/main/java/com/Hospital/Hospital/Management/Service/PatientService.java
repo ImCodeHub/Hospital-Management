8dd1be3f4c9322cad4dfebbafa14a1dd2acd9056
Ankit <ankitsharma.as420@gmail.com>
2024-07-03 23:15:32 +0530

    created interface for patient service

---
+++ src/main/java/com/Hospital/Hospital/Management/Service/PatientService.java
@@ -0,0 +1,7 @@
+package com.Hospital.Hospital.Management.Service;
+
+import com.Hospital.Hospital.Management.Entity.Patient;
+
+public interface PatientService {
+    public String savePatient(Patient patient);
+} 
