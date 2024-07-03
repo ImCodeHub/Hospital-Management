c7265c98571ebe719d0c3852f5b1738a80a61fcc
Ankit <ankitsharma.as420@gmail.com>
2024-07-03 23:15:01 +0530

    created patient controller

---
+++ src/main/java/com/Hospital/Hospital/Management/Controller/PatientController.java
@@ -0,0 +1,8 @@
+package com.Hospital.Hospital.Management.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("api/v1/")
+public class PatientController {
+    
+}
