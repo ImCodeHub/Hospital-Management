e79141422449af675cec035c1db604bd49282e8a
Ankit <ankitsharma.as420@gmail.com>
2024-07-08 03:42:27 +0530

    created a model to register the user

---
+++ src/main/java/com/Hospital/Hospital/Management/Model/RegisterRequest.java
@@ -0,0 +1,41 @@
+package com.Hospital.Hospital.Management.Model;
+
+import java.time.LocalDate;
+
+import com.Hospital.Hospital.Management.Entity.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    
+    private String userId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String gender;
+
+    private String email;
+
+    private String mobileNo;
+
+    private String password;
+
+    private String speciality;
+
+    private String discription;
+
+    private LocalDate dateOfJoining;
+
+    private String address;
+
+    private Role role;
+}
