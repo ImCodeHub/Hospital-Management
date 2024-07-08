126f9c0bc375b8c9cf1b6cdfd4719db738948d03
Ankit <ankitsharma.as420@gmail.com>
2024-07-08 23:31:41 +0530

    deleted the non-usable services

--- src/main/java/com/Hospital/Hospital/Management/Service/PatientService.java
+++
@@ -1,7 +0,0 @@
-package com.Hospital.Hospital.Management.Service;
-
-import com.Hospital.Hospital.Management.Entity.Patient;
-
-public interface PatientService {
-    public String savePatient(Patient patient);
-} 
