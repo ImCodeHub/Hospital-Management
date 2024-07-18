b63381a80007175094c69067e6a401cbcad9dbee
Ankit <ankitsharma.as420@gmail.com>
2024-07-18 11:18:05 +0530

    mention @CrossOrigin annotation to the controller for access API from frontend

--- src/main/java/com/Hospital/Hospital/Management/Controller/AuthController.java
+++ src/main/java/com/Hospital/Hospital/Management/Controller/AuthController.java
@@ -3,6 +3,7 @@ package com.Hospital.Hospital.Management.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -18,6 +19,7 @@ import com.Hospital.Hospital.Management.Service.Authenticate.TokenBlacklistServi
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
