8fd9ec2a6c7e9bb35a1831a8a7b118a4c7958c41
Ankit <ankitsharma.as420@gmail.com>
2024-07-18 11:20:26 +0530

    added new id with auto generated value for orgnazing data in DB

--- src/main/java/com/Hospital/Hospital/Management/Entity/Appointment.java
+++ src/main/java/com/Hospital/Hospital/Management/Entity/Appointment.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -19,27 +21,40 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name="appoinment")
+@Table(name="appointment")
 public class Appointment {
     @Id
-    @Column(name="appoinment_id")
-    private String appoinmentId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="appointment_id")
+    private String appointmentId;
 
     @ManyToOne
     @JoinColumn(name="patient_id")
     private User patient;
 
+    private String userName;
+
     private String reason;
+
+    private LocalDate dateOfBirth;
 
     @ManyToOne
     @JoinColumn(name="doctor_id")
     private User doctor;
 
-    @Column(name="appoinment_date")
-    private LocalDate appoinmentDate;
+    @Column(name="appointment_date")
+    private LocalDate appointmentDate;
 
     @Column(name="date_of_booking")
     private LocalDate dateOfBooking;
+
+    @Column(name = "appointment_time")
+    private String appointmentTime;
+
+    @Column(name="location")
+    private String location;
 
     @Enumerated(EnumType.STRING)
     private AppointmentStatus status;
