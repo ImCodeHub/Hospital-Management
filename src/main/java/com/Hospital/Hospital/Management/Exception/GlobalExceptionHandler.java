c32d03d7e0ff63b5ee354487c913955db04fe995
Ankit <ankitsharma.as420@gmail.com>
2024-07-18 11:27:08 +0530

    created new inner class to handle error messages.

--- src/main/java/com/Hospital/Hospital/Management/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/Hospital/Hospital/Management/Exception/GlobalExceptionHandler.java
@@ -16,38 +16,61 @@ import com.Hospital.Hospital.Management.Exception.CustomException.UserNotFoundEx
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    public static class ErrorResponse{
+        private String message;
+
+        public ErrorResponse(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
     
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException exception , WebRequest request){
-        return new ResponseEntity<>(exception.getMessage(),HttpStatus.NOT_FOUND);
+        ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
+        return new ResponseEntity<>(errorResponse,HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InvalidEmailException.class)
     public ResponseEntity<?> handleInvalidEmailException(InvalidEmailException exception, WebRequest request){
-        return new ResponseEntity<>(exception.getMessage(),HttpStatus.BAD_REQUEST);
+        ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
+        return new ResponseEntity<>(errorResponse,HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(EmailAlreadyExistException.class)
     public ResponseEntity<?> handleEmailAlreadyExistException(EmailAlreadyExistException exception, WebRequest request){
-        return new ResponseEntity<>(exception.getMessage(),HttpStatus.CONFLICT);
+        ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
+        return new ResponseEntity<>(errorResponse,HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(MobileNoAlreadyExistException.class)
     public ResponseEntity<?> handleMobileNoAlreadyExistException(MobileNoAlreadyExistException exception, WebRequest request){
-        return new ResponseEntity<>(exception.getMessage(),HttpStatus.CONFLICT);
+        ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
+        return new ResponseEntity<>(errorResponse,HttpStatus.CONFLICT);
     }
     
     @ExceptionHandler(IncorrectPasswordException.class)
     public ResponseEntity<?> handleIncorrectPasswordException(IncorrectPasswordException exception, WebRequest request){
-        return new ResponseEntity<>(exception.getMessage(),HttpStatus.UNAUTHORIZED);
+        ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
+        return new ResponseEntity<>(errorResponse,HttpStatus.UNAUTHORIZED);
     }
     @ExceptionHandler(AppoinmentNotBookedException.class)
     public ResponseEntity<?> handleAppoinmentNotBookedException(AppoinmentNotBookedException exception, WebRequest request){
-        return new ResponseEntity<>(exception.getMessage(),HttpStatus.UNAUTHORIZED);
+        ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
+        return new ResponseEntity<>(errorResponse,HttpStatus.UNAUTHORIZED);
     }
     @ExceptionHandler(AppoinmentNotFoundException.class)
     public ResponseEntity<?> handleAppoinmentNotFoundException(AppoinmentNotFoundException exception, WebRequest request){
-        return new ResponseEntity<>(exception.getMessage(),HttpStatus.UNAUTHORIZED);
+        ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
+        return new ResponseEntity<>(errorResponse,HttpStatus.UNAUTHORIZED);
     }
 
 }
