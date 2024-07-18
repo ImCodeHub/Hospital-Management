5a805ffa9b0dd9fbf931cab73b5b01ee1af2b0a7
Ankit <ankitsharma.as420@gmail.com>
2024-07-18 11:35:21 +0530

    renamed this file

---
+++ src/main/java/com/Hospital/Hospital/Management/Repository/AppointmentRepository.java
@@ -0,0 +1,19 @@
+package com.Hospital.Hospital.Management.Repository;
+
+import java.util.*;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.Hospital.Hospital.Management.Entity.Appointment;
+
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    public Optional<Appointment> findByAppointmentId(String id);
+
+    @Query(value = "SELECT * FROM Appointment WHERE YEAR(appointment_date)= :year AND MONTH(appointment_date)= :month AND (patient_id = :userId OR doctor_id = :userId )", nativeQuery = true)
+    public List<Appointment> findByYearAndMonth(@Param("year") int year, @Param("month") int month, @Param("userId") String userId);
+}
