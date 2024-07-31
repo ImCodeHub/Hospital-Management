5c487b15253626caa9260adbda9450e15d9bb533
Ankit <ankitsharma.as420@gmail.com>
2024-07-31 22:24:57 +0530

    added bean for java mail sender to configuration email details

--- src/main/java/com/Hospital/Hospital/Management/Configuration/ApplicationConfiguration.java
+++ src/main/java/com/Hospital/Hospital/Management/Configuration/ApplicationConfiguration.java
@@ -1,8 +1,13 @@
 package com.Hospital.Hospital.Management.Configuration;
 
+import java.util.Properties;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -22,6 +27,33 @@ public class ApplicationConfiguration {
 
     @Autowired
     private final UserRepository userRepository;
+
+    @Value("${spring.mail.host}")
+    private String mailHost;
+
+    @Value("${spring.mail.port}")
+    private String mailPort;
+    
+    @Value("${spring.mail.username}")
+    private String mailUsername;
+
+    @Value("${spring.mail.password}")
+    private String mailPassword;
+
+    @Bean
+    public JavaMailSender getJavaMailSender(){
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setHost(mailHost);
+        javaMailSender.setPort(Integer.parseInt(mailPort));
+        javaMailSender.setUsername(mailUsername);
+        javaMailSender.setPassword(mailPassword);
+
+        Properties props = javaMailSender.getJavaMailProperties();
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return javaMailSender;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
