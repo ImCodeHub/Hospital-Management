ff2b37135292266a4440cc9c72511bd3338db602
Ankit <ankitsharma.as420@gmail.com>
2024-08-02 16:24:11 +0530

    created new endpoint in controller for register doctor

--- src/main/java/com/Hospital/Hospital/Management/Controller/AdminController.java
+++ src/main/java/com/Hospital/Hospital/Management/Controller/AdminController.java
@@ -6,11 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.Hospital.Hospital.Management.Model.DoctorRegisterationModel;ller;
 
 import com.Hospital.Hospital.Management.Entity.User;
 import com.Hospital.Hospital.Management.Service.Implement.AdminServiceImpl;
@@ -21,6 +26,13 @@ import com.Hospital.Hospital.Management.Service.Implement.AdminServiceImpl;
 public class AdminController {
     @Autowired
     private AdminServiceImpl adminServiceImpl;
+
+
+    @PostMapping("registerDoctor")
+    public ResponseEntity<String> register(@RequestPart DoctorRegisterationModel doctorModel, @RequestPart MultipartFile imageFile ){
+        String response = adminServiceImpl.registerDoctor(doctorModel, imageFile);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 
     @GetMapping("doctors")
     public ResponseEntity<List<User>> getDoctorList(){
