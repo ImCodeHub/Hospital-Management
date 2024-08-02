c180abd683768035d4d5f3f53036b147bec7a96a
Ankit <ankitsharma.as420@gmail.com>
2024-08-02 16:26:33 +0530

    created a doctor model

---
+++ src/main/java/com/Hospital/Hospital/Management/Model/DoctorRegisterationModel.java
@@ -0,0 +1,44 @@
+package com.Hospital.Hospital.Management.Model;
+
+import java.time.LocalDate;
+
+import com.Hospital.Hospital.Management.Entity.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DoctorRegisterationModel {
+    private String userId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String gender;
+
+    private String email;
+
+    private String mobileNo;
+
+    private String password;
+
+    private String discription;
+
+    private LocalDate dateOfJoining;
+
+    private Role role;
+
+    private String photo;
+
+    private String education;
+
+    private String specialization;
+
+    private Long experience;
+
+    private String about;
+}
