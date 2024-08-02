c0316ce17ea26ffd147d659c325e4e5cf40553d6
Ankit <ankitsharma.as420@gmail.com>
2024-08-02 16:27:21 +0530

    add method

--- src/main/java/com/Hospital/Hospital/Management/Service/AdminService.java
+++ src/main/java/com/Hospital/Hospital/Management/Service/AdminService.java
@@ -2,10 +2,14 @@ package com.Hospital.Hospital.Management.Service;
 
 import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.Hospital.Hospital.Management.Entity.User;
+import com.Hospital.Hospital.Management.Model.DoctorRegisterationModel;
 
 public interface AdminService {
 
+    public String registerDoctor(DoctorRegisterationModel doctorModel, MultipartFile imageFile);
     public List<User> getAllDoctors();
     public List<User> getAllPatient();
     public Boolean blacklist(String userId);
