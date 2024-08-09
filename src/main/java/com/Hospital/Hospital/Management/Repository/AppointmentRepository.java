cd7651068a18102af410764dadaeaf5a6e7c9972
Ankit <ankitsharma.as420@gmail.com>
2024-08-09 09:47:10 +0530

    added new query for findByappointment id

--- src/main/java/com/Hospital/Hospital/Management/Repository/AppointmentRepository.java
+++ src/main/java/com/Hospital/Hospital/Management/Repository/AppointmentRepository.java
@@ -12,8 +12,11 @@ import com.Hospital.Hospital.Management.Entity.Appointment;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-    public Optional<Appointment> findByAppointmentId(String id);
+
+    @Query(value="SELECT * FROM Appointment WHERE appointment_id=:id", nativeQuery = true)
+    public Optional<Appointment> findByAppointmentId(@Param("id") String id);
 
     @Query(value = "SELECT * FROM Appointment WHERE YEAR(appointment_date)= :year AND MONTH(appointment_date)= :month AND (patient_id = :userId OR doctor_id = :userId )", nativeQuery = true)
     public List<Appointment> findByYearAndMonth(@Param("year") int year, @Param("month") int month, @Param("userId") String userId);
+
 }
