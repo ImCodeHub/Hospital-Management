18c88a4b5a1c7875cf49f4cb3a8e438e8b16cfa4
Ankit <ankitsharma.as420@gmail.com>
2024-08-09 09:16:46 +0530

    added type field for dropdown and join column one to one doctor

--- src/main/java/com/Hospital/Hospital/Management/Entity/Profile.java
+++ src/main/java/com/Hospital/Hospital/Management/Entity/Profile.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,8 +27,14 @@ public class Profile {
 
     private String specialization;
 
+    private String type;
+
     private Long experience;
 
     private String about;
+
+    @OneToOne
+    @JoinColumn(name="user_id")
+    private User user;
 
 }
